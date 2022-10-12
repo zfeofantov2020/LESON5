489d4a2b5a2a17e45b265330f96c24b4cb2944fa
Eugene <zfeofantov@gmail.com>
2022-10-12 23:03:35 +0100

    SECOND TEST

--- Program.cs
+++ Program.cs
@@ -6,22 +6,22 @@
 
 // 2, 4 -> 16
 
-// int ssa(int numberA, int numberB)
-// {
-//   int result = 1;
-//   for(int i=1; i <= numberB; i++)
-//   {
-//     result = result * numberA;
-//   }
-//     // int result = Math.Pow(numberA, numberB);
-//     return result;
-// }
+int ssa(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
 
-//   Console.Write("Введите число A: ");
-//   int numberA = Convert.ToInt32(Console.ReadLine());
-//   Console.Write("Введите число B: ");
-//   int numberB = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
 
-//   int exponentiation = ssa(numberA, numberB);
-//   Console.WriteLine("Ответ: " + exponentiation);
+  int exponentiation = ssa(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
 
